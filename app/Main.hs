b3df9d0357d99c23280bb477a5e4081b1dd9d212
permalik <permalik@protonmail.com>
2024-10-23 22:17:07 -0500

    feat: impl cmli arg input

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,28 @@
 module Main where
 
--- import qualified System.Environment as Env
 import Lexer (lexText)
 import Parser (parseText)
+import qualified System.Environment as Env
+
+handleArgs :: IO (Either String FilePath)
+handleArgs = parseArgs <$> Env.getArgs
+  where
+    parseArgs argumentList =
+        case argumentList of
+            [fname] -> Right fname
+            [] -> Left "error: no arguments provided"
+            _ -> Left "error: multiple files not supported"
 
 main :: IO ()
-main = do
-    putStrLn "Hello, Haskell!"
-    Lexer.lexText
-    Parser.parseText
+main =
+    handleArgs >>= displayMessage
+  where
+    displayMessage parsedArgument =
+        case parsedArgument of
+            Left errMessage ->
+                putStrLn $ "Error: " <> errMessage
+            Right fileName ->
+                putStrLn $ "Opening File:" <> fileName
+
+-- Lexer.lexText
+-- Parser.parseText
