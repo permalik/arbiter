f8c2a2fb1ad96fc7f3ec14efede89a67d777940d
permalik <permalik@protonmail.com>
2024-10-31 19:48:25 -0500

    feat: get cli args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let file_path: &String;
+    if args.len() > 1 {
+        file_path = &args[1];
+
+        println!("{}", file_path);
+    } else {
+        println!("usage: cargo run -- file_path.md");
+    }
 }
