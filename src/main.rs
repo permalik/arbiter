a0b2001be5afba9b4f67e35236295f788356e9ff
permalik <permalik@protonmail.com>
2024-11-06 06:45:38 -0600

    refactor: read lines from test.md

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,53 @@
-mod elements;
-mod lexer;
+//mod elements;
+//mod lexer;
 
-use lexer::lexer::parse;
-use std::env;
-use std::error::Error;
-use std::fs;
+//use lexer::lexer::parse;
+//use std::error::Error;
+//use std::fs::File;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+use std::path::Path;
+//use std::{env, io};
+//use std::env;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let file_path = parse_args(&args);
+    //let args: Vec<String> = env::args().collect();
+    //let file_path = parse_args(&args);
 
-    match read_file(file_path) {
-        Ok(file_contents) => {
-            let file_contents_str: &str = &file_contents;
-            parse(file_contents_str);
+    if let Ok(lines) = read_lines("./test.md") {
+        for line in lines.flatten() {
+            println!("{}", line);
         }
-        Err(e) => eprintln!("error: {e}"),
     }
+
+    //match read_file(file_path) {
+    //    Ok(file_contents) => {
+    //        let file_contents_str: &str = &file_contents;
+    //        parse(file_contents_str);
+    //    }
+    //    Err(e) => eprintln!("error: {e}"),
+    //}
 }
 
-fn parse_args(args: &[String]) -> Option<&str> {
-    if args.len() > 1 {
-        Some(&args[1])
-    } else {
-        None
-    }
+//fn parse_args(args: &[String]) -> Option<&str> {
+//    if args.len() > 1 {
+//        Some(&args[1])
+//    } else {
+//        None
+//    }
+//}
+
+fn read_lines<P>(file_path: P) -> io::Result<io::Lines<BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(file_path)?;
+    Ok(BufReader::new(file).lines())
 }
 
-fn read_file(file_path: Option<&str>) -> Result<String, Box<dyn Error>> {
-    match file_path {
-        Some(path) => fs::read_to_string(path).map_err(|e| e.into()),
-        None => Err("no file path provided".into()),
-    }
-}
+//fn read_file(file_path: Option<&str>) -> Result<String, Box<dyn Error>> {
+//    match file_path {
+//        Some(path) => fs::read_to_string(path).map_err(|e| e.into()),
+//        None => Err("no file path provided".into()),
+//    }
+//}
