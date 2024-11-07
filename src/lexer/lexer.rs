74cbce7a94dd11d689ff8453a6bbb3004c897c5b
permalik <permalik@protonmail.com>
2024-11-07 06:31:40 -0600

    refactor: input lines rather full file as string

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -6,8 +6,8 @@ pub struct Token {
     position: usize,
 }
 
-pub fn parse(file_contents: &str) {
-    for token in tokenize(file_contents) {
+pub fn parse(line: &str) {
+    for token in lex(line) {
         println!(
             "Token: kind: {:?}, value: {:?}, position: {:?}",
             token.kind, token.value, token.position
@@ -15,19 +15,19 @@ pub fn parse(file_contents: &str) {
     }
 }
 
-pub fn tokenize(file_contents: &str) -> Vec<Token> {
+pub fn lex(line: &str) -> Vec<Token> {
     let mut tokens: Vec<Token> = Vec::new();
     let mut i = 0;
 
-    while i < file_contents.len() {
-        assert!(i < file_contents.len(), "assert: index oob");
-        let c = file_contents.chars().nth(i).unwrap();
+    while i < line.len() {
+        assert!(i < line.len(), "assert: index oob");
+        let c = line.chars().nth(i).unwrap();
         match c {
             '#' => {
                 let mut heading_level = 1;
 
-                while i + heading_level < file_contents.len()
-                    && file_contents.chars().nth(i + heading_level) == Some('#')
+                while i + heading_level < line.len()
+                    && line.chars().nth(i + heading_level) == Some('#')
                 {
                     heading_level += 1;
                 }
@@ -93,7 +93,7 @@ pub fn tokenize(file_contents: &str) -> Vec<Token> {
                 let mut line_break_position = 1;
 
                 while line_break_position < 3
-                    && file_contents.chars().nth(i + line_break_position) == Some(' ')
+                    && line.chars().nth(i + line_break_position) == Some(' ')
                 {
                     line_break_position += 1;
                     if line_break_position == 3 {
@@ -153,3 +153,142 @@ pub fn tokenize(file_contents: &str) -> Vec<Token> {
     );
     tokens
 }
+
+//pub fn lex(file_contents: &str) -> Vec<Token> {
+//    let mut tokens: Vec<Token> = Vec::new();
+//    let mut i = 0;
+//
+//    while i < file_contents.len() {
+//        assert!(i < file_contents.len(), "assert: index oob");
+//        let c = file_contents.chars().nth(i).unwrap();
+//        match c {
+//            '#' => {
+//                let mut heading_level = 1;
+//
+//                while i + heading_level < file_contents.len()
+//                    && file_contents.chars().nth(i + heading_level) == Some('#')
+//                {
+//                    heading_level += 1;
+//                }
+//
+//                if heading_level <= 6 {
+//                    let (_heading_literal, token_kind) = match heading_level {
+//                        1 => {
+//                            let heading_one = literals::HEADING_ONE;
+//                            if let Tokens::HeadingOne(ref heading_one_literal) = heading_one {
+//                                assert_eq!(heading_one_literal, &"#".repeat(heading_level));
+//                            }
+//                            (heading_one, Tokens::HeadingOne("#"))
+//                        }
+//                        2 => {
+//                            let heading_two = literals::HEADING_TWO;
+//                            if let Tokens::HeadingTwo(ref heading_two_literal) = heading_two {
+//                                assert_eq!(heading_two_literal, &"#".repeat(heading_level));
+//                            }
+//                            (heading_two, Tokens::HeadingTwo("##"))
+//                        }
+//                        3 => {
+//                            let heading_three = literals::HEADING_THREE;
+//                            if let Tokens::HeadingThree(ref heading_three_literal) = heading_three {
+//                                assert_eq!(heading_three_literal, &"#".repeat(heading_level));
+//                            }
+//                            (heading_three, Tokens::HeadingThree("###"))
+//                        }
+//                        4 => {
+//                            let heading_four = literals::HEADING_FOUR;
+//                            if let Tokens::HeadingFour(ref heading_four_literal) = heading_four {
+//                                assert_eq!(heading_four_literal, &"#".repeat(heading_level));
+//                            }
+//                            (heading_four, Tokens::HeadingFour("####"))
+//                        }
+//                        5 => {
+//                            let heading_five = literals::HEADING_FIVE;
+//                            if let Tokens::HeadingFive(ref heading_five_literal) = heading_five {
+//                                assert_eq!(heading_five_literal, &"#".repeat(heading_level));
+//                            }
+//                            (heading_five, Tokens::HeadingFive("#####"))
+//                        }
+//                        6 => {
+//                            let heading_six = literals::HEADING_SIX;
+//                            if let Tokens::HeadingSix(ref heading_six_literal) = heading_six {
+//                                assert_eq!(heading_six_literal, &"#".repeat(heading_level));
+//                            }
+//                            (heading_six, Tokens::HeadingSix("######"))
+//                        }
+//                        _ => unreachable!(),
+//                    };
+//
+//                    tokens.push(Token {
+//                        kind: token_kind,
+//                        value: format!("{}", "#".repeat(heading_level)),
+//                        position: i,
+//                    });
+//                }
+//                i += heading_level;
+//                continue;
+//            }
+//            '\n' => {
+//                let mut is_line_break = false;
+//                let mut line_break_position = 1;
+//
+//                while line_break_position < 3
+//                    && file_contents.chars().nth(i + line_break_position) == Some(' ')
+//                {
+//                    line_break_position += 1;
+//                    if line_break_position == 3 {
+//                        is_line_break = true;
+//                    }
+//                }
+//
+//                if is_line_break {
+//                    let line_break = literals::LINE_BREAK;
+//                    if let Tokens::LineBreak(ref line_break_literal) = line_break {
+//                        assert_eq!(line_break_literal, &"  \n");
+//                        tokens.push({
+//                            Token {
+//                                kind: Tokens::LineBreak("  \n"),
+//                                value: line_break_literal.to_string(),
+//                                position: i,
+//                            }
+//                        })
+//                    }
+//                } else {
+//                    let newline = literals::NEWLINE;
+//                    if let Tokens::Newline(ref newline_literal) = newline {
+//                        assert_eq!(newline_literal, &"\n");
+//                        tokens.push({
+//                            Token {
+//                                kind: Tokens::Newline("\n"),
+//                                value: newline_literal.to_string(),
+//                                position: i,
+//                            }
+//                        });
+//                    }
+//                }
+//                i += 1;
+//            }
+//            ' ' => {
+//                let space = literals::SPACE;
+//                if let Tokens::Space(ref space_literal) = space {
+//                    assert_eq!(space_literal, &" ");
+//                    tokens.push({
+//                        Token {
+//                            kind: Tokens::Space(" "),
+//                            value: space_literal.to_string(),
+//                            position: i,
+//                        }
+//                    });
+//                }
+//                i += 1;
+//            }
+//            _ => {
+//                i += 1;
+//            }
+//        }
+//    }
+//    assert!(
+//        i > 0,
+//        "assert: index i did not advance, potential infinite loop"
+//    );
+//    tokens
+//}
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,29 @@
-//mod elements;
-//mod lexer;
+mod elements;
+mod lexer;
 
-//use lexer::lexer::parse;
+use lexer::lexer::parse;
 //use std::error::Error;
 //use std::fs::File;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 use std::path::Path;
 //use std::{env, io};
-//use std::env;
+use std::env;
 
 fn main() {
-    //let args: Vec<String> = env::args().collect();
-    //let file_path = parse_args(&args);
+    let args: Vec<String> = env::args().collect();
+    let file_path = parse_args(&args);
 
-    if let Ok(lines) = read_lines("./test.md") {
-        for line in lines.flatten() {
-            println!("{}", line);
+    match file_path {
+        Some(path) => {
+            if let Ok(lines) = read_lines(path.to_string()) {
+                for line in lines.flatten() {
+                    parse(&line);
+                }
+            }
         }
+        None => panic!("no file path provided"),
     }
-
     //match read_file(file_path) {
     //    Ok(file_contents) => {
     //        let file_contents_str: &str = &file_contents;
@@ -29,13 +33,13 @@ fn main() {
     //}
 }
 
-//fn parse_args(args: &[String]) -> Option<&str> {
-//    if args.len() > 1 {
-//        Some(&args[1])
-//    } else {
-//        None
-//    }
-//}
+fn parse_args(args: &[String]) -> Option<&str> {
+    if args.len() > 1 {
+        Some(&args[1])
+    } else {
+        None
+    }
+}
 
 fn read_lines<P>(file_path: P) -> io::Result<io::Lines<BufReader<File>>>
 where
@@ -44,10 +48,3 @@ where
     let file = File::open(file_path)?;
     Ok(BufReader::new(file).lines())
 }
-
-//fn read_file(file_path: Option<&str>) -> Result<String, Box<dyn Error>> {
-//    match file_path {
-//        Some(path) => fs::read_to_string(path).map_err(|e| e.into()),
-//        None => Err("no file path provided".into()),
-//    }
-//}
